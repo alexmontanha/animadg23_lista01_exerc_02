ebbae8491011eb153f8c2c9804446a14907f1f99
Alexandre de Oliveira <alexmontanha@hotmail.com>
2023-04-03 20:48:44 -0300

    Fechar teclado para evitar memory leak

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ public class App {
         //Entradas: Um número
         Scanner teclado = new Scanner(System.in);
         int numero = teclado.nextInt();
+        teclado.close();
         //Processamento: Não tem
         //Saída: O número informado foi [número]
         System.out.println("O número informado foi [" + numero + "]");
